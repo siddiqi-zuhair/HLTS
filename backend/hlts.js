bf2543bef0f3084a338b5c0d715895a893ba5160
Chujji <siddiqizuhair@gmail.com>
2021-12-25 01:13:54 -0500

    Changing from typescript to javascript

---
+++ backend/hlts.js
@@ -0,0 +1,6 @@
+let hltb = require('howlongtobeat');
+
+let hltbService = new HowLongToBeatService();
+//hltbService.search('Kingdom Hearts').then(result => console.log(result));
+const apikey = "416B1421E0EA403BA18B9D6F4977E010"
+const steamid = "76561198976808628"
--- backend/hlts.ts
+++
@@ -1,4 +0,0 @@
-import { HowLongToBeatService, HowLongToBeatEntry } from 'howlongtobeat';
-
-let hltbService = new HowLongToBeatService();
-hltbService.search('Nioh').then(result => console.log(result));
