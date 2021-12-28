de3603ccca1e7d7d9e89ffd6ced5da9f56c1c311
Chujji <siddiqizuhair@gmail.com>
2021-12-27 22:51:12 -0500

    Added .gitignore

--- backend/hlts.js
+++ backend/hlts.js
@@ -1,6 +1,8 @@
 let hltb = require('howlongtobeat');
-
-let hltbService = new HowLongToBeatService();
+let hltbService = new hltb.HowLongToBeatService();
+const SteamAPI = require('steamapi');
+const steam = new SteamAPI('steam token');
+require('dotenv').config()
 //hltbService.search('Kingdom Hearts').then(result => console.log(result));
 const apikey = "416B1421E0EA403BA18B9D6F4977E010"
 const steamid = "76561198976808628"
