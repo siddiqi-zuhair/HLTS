7639b2723871959ee8116e4a44825d55a0847228
Chujji <siddiqizuhair@gmail.com>
2021-12-27 22:56:36 -0500

    Fixed gitignore

--- backend/hlts.js
+++ backend/hlts.js
@@ -4,5 +4,5 @@ const SteamAPI = require('steamapi');
 const steam = new SteamAPI('steam token');
 require('dotenv').config()
 //hltbService.search('Kingdom Hearts').then(result => console.log(result));
-const apikey = "416B1421E0EA403BA18B9D6F4977E010"
+const apikey = process.env.API_KEY; 
 const steamid = "76561198976808628"
