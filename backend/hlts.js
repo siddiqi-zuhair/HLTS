5746af4fd9dffed30f4c6df9e2b02d6ff1aafbdc
Chujji <siddiqizuhair@gmail.com>
2021-12-27 23:57:30 -0500

    Hid API key

--- backend/hlts.js
+++ backend/hlts.js
@@ -1,9 +1,10 @@
 let hltb = require('howlongtobeat');
 let hltbService = new hltb.HowLongToBeatService();
 const SteamAPI = require('steamapi');
-const steam = new SteamAPI('steam token');
 require('dotenv').config()
+//console.log(process.env);
+apiKey = process.env.API_KEY 
+const steam = new SteamAPI(apiKey);
+//steam.resolve('https://steamcommunity.com/id/ILOOVEYOU').then(id = steamid)
+//console.log(steamid) 
 //hltbService.search('Kingdom Hearts').then(result => console.log(result));
-const apikey = process.env.API_KEY; 
-const steamid = "76561198976808628"
-console.log("Test"); 
